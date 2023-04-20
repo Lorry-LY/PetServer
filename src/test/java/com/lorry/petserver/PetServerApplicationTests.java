465dfcbe2710ff044dedfe2c749fec9ce6c2c4d9
Lorry <ly3124675luces@163.com>
2023-04-20 21:26:29 +0800

    搭建工程

---
+++ src/test/java/com/lorry/petserver/PetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorry.petserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
